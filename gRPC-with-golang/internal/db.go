af88014220e05d89314cbe3c09800947ab3b4935
Govind Shukla <govind.s@turvo.com>
2024-08-09 17:54:24 +0530

    added more changes

--- gRPC-with-golang/internal/db.go
+++ gRPC-with-golang/internal/db.go
@@ -16,9 +16,36 @@ func NewDB() *DB {
 func (db *DB) AddOrder(order *orders.Order) error {
 	for _, o := range db.collection {
 		if o.GetOrderId() == order.GetOrderId() {
-			return fmt.Errorf("order already exists with order id %d", order.GetOrderId())
+			return fmt.Errorf("duplicate order id: %d", order.GetOrderId())
 		}
 	}
 	db.collection = append(db.collection, order)
 	return nil
+}
+
+func (db *DB) GetOrder(orderId uint64) (*orders.PayloadWithSingleOrder, error) {
+	for _, o := range db.collection {
+		if o.GetOrderId() == orderId {
+			return &orders.PayloadWithSingleOrder{Order: o}, nil
+		}
+	}
+	return nil, fmt.Errorf("order not found")
+}
+
+func (db *DB) UpdateOrder(order *orders.Order) bool {
+	for i, o := range db.collection {
+		if o.GetOrderId() == order.GetOrderId() {
+			db.collection[i] = order
+			return true
+		}
+	}
+	return false
+}
+
+func (db *DB) RemoveOrder(orderId uint64) {
+	for i, o := range db.collection {
+		if o.GetOrderId() == orderId {
+			db.collection = append(db.collection[:i], db.collection[i+1:]...)
+		}
+	}
 }
--- gRPC-with-golang/internal/orderservice.go
+++ gRPC-with-golang/internal/orderservice.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"errors"
 	"gRPC-with-golang/protogen/golang/orders"
 	"log"
 )
@@ -14,10 +15,31 @@ type OrderService struct {
 func NewOrderService(db *DB) OrderService {
 	return OrderService{db: db}
 }
-func (o *OrderService) AddOrder(ctx context.Context, req *orders.PayloadWithSingleOrder) (*orders.Empty, error) {
+
+func (o *OrderService) AddOrder(_ context.Context, req *orders.PayloadWithSingleOrder) (*orders.Empty, error) {
 	log.Printf("Adding order with payload: %v", req)
 
 	err := o.db.AddOrder(req.GetOrder())
 
 	return &orders.Empty{}, err
+}
+
+func (o *OrderService) GetOrder(_ context.Context, req *orders.PayloadWithOrderId) (*orders.PayloadWithSingleOrder, error) {
+	log.Printf("Getting order with payload: %v", req)
+	return o.db.GetOrder(req.GetOrderId())
+}
+
+func (o *OrderService) UpdateOrder(_ context.Context, req *orders.PayloadWithSingleOrder) (*orders.Empty, error) {
+	log.Printf("Updating order with payload: %v", req)
+	isUpdated := o.db.UpdateOrder(req.GetOrder())
+	if isUpdated {
+		return &orders.Empty{}, nil
+	}
+	return &orders.Empty{}, errors.New("order not found")
+}
+
+func (o *OrderService) RemoveOrder(_ context.Context, req *orders.PayloadWithOrderId) (*orders.Empty, error) {
+	log.Printf("Deleting order with id: %d", req)
+	o.db.RemoveOrder(req.GetOrderId())
+	return &orders.Empty{}, nil
 }
